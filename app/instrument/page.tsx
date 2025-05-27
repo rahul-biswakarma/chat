97eac76a526c0a488ea13fd2ec6a98b18d91e40c
Rahul Biswakarma <rahul.id39@gmail.com>
2025-05-27 05:54:35 +0530

    refactor: Remove .npmrc file, update next.config.ts for static export settings, and add new deployment scripts in package.json. Enhance package-lock.json with additional dependencies for improved functionality.

--- app/instrument/page.tsx
+++
@@ -1,7 +0,0 @@
-import { createSupabaseServerClient } from "@/supabase/client/server";
-
-export default async function Instruments() {
-  const supabase = await createSupabaseServerClient();
-  const { data: instruments } = await supabase.from("instruments").select();
-  return <pre>{JSON.stringify(instruments, null, 2)}</pre>;
-}
